70d001b9fb0024ea974d3ff11d78b1d2105ed73a
erickromorodriguez29 <erick.romo.rodriguez29@gmail.com>
2025-08-25 12:36:31 -0600

    Update fizzbuzz.js
    Erick romo

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,17 @@
-export function fizzbuzz(){
-  //tu código va aqui
+export function fizzbuzz(1,100){
+    for(let i=1; i<= numero; 1++){
+        if ((i %3===0) && (i % 5 === 0)){
+            console.log('fizzbuzz')
+        }
+        else if ( i % 3 ===0 ){
+            console.log('fizz')
+        }
+        else if (i % 5 ===0 ){
+            console.log('buzz')
+        }
+        else {console.log('imput: ${i}')}
+    }
+}
+
   
 }
